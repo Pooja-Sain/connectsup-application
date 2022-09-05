f2c835693756b28357471bae1d2f80393bfcce51
PoojaSain6 <sainpooja7988@gmail.com>
2022-09-05 10:48:07 +0530

    profile module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { ProfileModule } from './profile/profile.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, ProfileModule, LoginModule,
     AppRoutingModule
   ],
   providers: [],
